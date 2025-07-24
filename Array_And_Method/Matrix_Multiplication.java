79fa656857ded152df3d6f37b4a596be4d19d700
Prashant30102005 <prashantparihar638@gmail.com>
2025-07-25 03:31:49 +0530

    JAVA Basics

--- Array_And_Method/Matrix_Multiplication.java
+++ Array_And_Method/Matrix_Multiplication.java
@@ -1,7 +1,7 @@
 package Array_And_Method;
 
 public class Matrix_Multiplication {
-    public static int[][] multi(int[][] a,int[][] b){
+    public int[][] multi(int[][] a,int[][] b){
         int[][] c = new int[a.length][b[0].length];
         for(int i =0;i<a.length;i++){
             for(int j =0;j<b[0].length;j++){
@@ -13,9 +13,10 @@ public class Matrix_Multiplication {
         return c;
     }
     public static void main(String[] args) {
+        Matrix_Multiplication m = new Matrix_Multiplication();
         int[][] a = {{1,2,3}};
         int[][] b = {{1},{2},{3}};
-        int[][] c = multi(a,b);
+        int[][] c = m.multi(a,b);
         for(int j =0;j<c.length;j++){
                 for(int k =0;k<c[0].length;k++){
                     System.out.printf("%d",c[j][k]);
